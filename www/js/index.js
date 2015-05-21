e88289fc5492227dc01b3ca89959edccb3851487
Dieter Holvoet <dieter.holvoet@gmail.com>
2015-05-21 15:41:13 +0200

    fixke

--- www/js/index.js
+++ www/js/index.js
@@ -6,10 +6,11 @@ jquery: true
 
 var events = [];
 
-function Event(code) {
+function Event(code, name) {
     this.code = "";
     this.code = code;
     this.name = "";
+    this.name = name;
     this.cover = "";
 }
 
@@ -27,11 +28,35 @@ function checkInvalidInput(element) {
         $(element).attr("placeholder", "Ongeldige invoer");
         return false;
         
+    } else if($(element).attr("name") === "code") {
+        return validateCode($(element).val());
+        
     } else {
         $(element).parent().removeClass("invalid-input");
         $(element).attr("placeholder", temp);
         return true;
     }
+}
+
+function validateCode(code) {
+    var result = true;
+    $.ajax({
+        url: 'crosscall.php',
+        data: {url: "http://api.adaytoshare.be/1/platform/check_code?code=" + code},
+        type: 'POST',
+        async: false,
+        success: function (data) {
+            data = JSON.parse(data);
+            if(data.success === 1) {
+                events.push(new Event(code, data.album_name));
+                console.log("New event added!")
+            } else {
+                console.error(data.error_message);
+                result = false;
+            }
+        }
+    });
+    return result;
 }
 
 $(document).ready(function() {
@@ -47,10 +72,13 @@ $(document).ready(function() {
     // Controle login
     $("#loginform").on("submit", function(e) {
         e.preventDefault();
+        var check_code = checkInvalidInput($("#login-code"));
+        var check_login = checkInvalidInput($("#login-naam"));
         
-        if (checkInvalidInput($("#login-code")) && checkInvalidInput($("#login-naam"))) {
+        if (check_code && check_login && validateCode($("#login-code").val())) {
             localStorage.setItem("username", $("#login-naam").val());
-            events[0] = new Event($("#login-code").val());
+            localStorage.setItem("events", JSON.stringify(events));
+            
             if(events.length > 1) {
                 $("body").pagecontainer("change", "#page-eventlist", {});
             } else {
