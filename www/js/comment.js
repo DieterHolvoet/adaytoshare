f29b8031a87c2c6bbd3caca913a430f5fdb1004c
Skitmark <vdbvincent_7@hotmail.com>
2015-05-21 16:04:44 +0200

    comment

--- www/js/comment.js
+++ www/js/comment.js
@@ -1,7 +1,16 @@
+
 $(document).ready(function() {
-	$( ".comment" ).click(function() {
+	$(".comment").click(function() {
   		$(this).next().slideToggle( "fast", function() {
     // Animation complete.
   	});
+	});
+
+	$(".partypoints").click(function(){
+		var n = $(".ppNumber").text();
+
+		n++;
+
+		$(".ppNumber").text(n);
 	});
 });
