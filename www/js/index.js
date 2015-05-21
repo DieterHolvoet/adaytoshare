1299c417830a9714219da4c51c910e8ebe799c6c
Dieter Holvoet <dieter.holvoet@gmail.com>
2015-05-21 10:18:56 +0200

    Index.js

--- www/js/index.js
+++ www/js/index.js
@@ -18,28 +18,32 @@ if(localStorage.getItem("events")) {
 }
 
 $(document).ready(function() {
+    
+    $("#loginform").on("submit", function(e) {
+        e.preventDefault();
+
+        var t_name = $("#login-naam").val();    // Waarde v.h. naam-veld
+        var t_code = $("#login-code").val();    // Waarde v.h. code-veld
+        
+        if(t_name === "") {
+            $(".icon-user185").addClass("invalid-input");
+        }
+        
+        if(t_code === "") {
+            $(".icon-locked59").addClass("invalid-input");
+
+        } else {
+            $(".icon-user185:before").removeClass("invalid-input");
+            $(".icon-locked59:before").removeClass("invalid-input");
+
+            localStorage.setItem("username", $("#login-name").value);
+            events[0] = new Event(t_code);
+        }
+    });
+    
     if(!localStorage.getItem("username")) {
         $("body").pagecontainer("change", "#page-login", {});
         
-        $("#loginform").on("submit", function(e) {
-            e.preventDefault();
-            
-            var t_name = $("#login-name").value;    // Waarde v.h. naam-veld
-            var t_code = $("#login-code").value;    // Waarde v.h. code-veld
-            
-            if(t_name === "") {
-                $(".icon-user185:before").css("color", "#e84746");
-                
-            } else {
-                if(t_code === "") {
-                    $(".icon-locked59:before").css("color", "#e84746");
-                    
-                } else {
-                    localStorage.setItem("username", $("#login-name").value);
-                    events[0] = new Event(t_code);
-                }
-            }
-        });
     } else {
         $("body").pagecontainer("change", "#page-login", {});
     }
