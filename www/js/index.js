b7f3e46d13d8e621c4f311ca53db9c7d9825f3c2
Dieter Holvoet <dieter.holvoet@gmail.com>
2015-05-27 15:20:47 +0200

    woepsie

--- www/js/index.js
+++ www/js/index.js
@@ -582,11 +582,11 @@ $(document).ready(function() {
 
         function onSuccess(imageData) {
             /*Functie android crop rights reserved Ben De Greef*/
-            if(/android/i.test(navigator.userAgent)){
-            var canvas = document.getElementById("canvas");
-            var context = canvas.getContext("2d");
-            var imageObj = document.getElementById("myImage");
-            var width;
+            if(/android/i.test(navigator.userAgent)) {
+            var canvas = document.getElementById("canvas"),
+                context = canvas.getContext("2d"),
+                imageObj = document.getElementById("myImage"),
+                width;
 
             imageObj.src = "data:image/jpeg;base64," + imageData;
 
@@ -625,7 +625,8 @@ $(document).ready(function() {
                     url: "http://api.adaytoshare.be/1/guestbook/post_with_media_base64",
                     data: "code=" + activeNewsfeed
                         + "&from=" + localStorage.getItem("username")
-                        + "&photo=" + $("#defImg").attr("src")
+                        + "&photo=" + $("#defImg").attr("src").replace("data:image/png;base64,", "")
+                        + "&extension=png"
                         + (hasMessage ? ("&message=" + $(".boodschap").val()) : "")
                         + (isPrivate ? ("&public=0") : ""),
                     dataType: 'json',
